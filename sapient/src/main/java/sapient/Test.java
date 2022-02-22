0c83c5536b3b01509574b0000c0d22929b5defe2
shailendra-3110 <shailendraparihar172@gmail.com>
2022-02-22 19:43:11 +0530

    first commit

---
+++ sapient/src/main/java/sapient/Test.java
@@ -0,0 +1,22 @@
+package sapient;
+import java.util.Scanner;
+
+public class Test {
+
+	
+		
+		public static void main(String[] args) {
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter first name");
+			String firstName=sc.next();
+
+			System.out.println("Enter last name");
+			String lastName=sc.next();
+
+
+			System.out.println("Hello " +firstName+" "+lastName);
+
+
+	}
+
+}
